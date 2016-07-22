5ab5533aec42c7e460740de26dfd2ee4b50f4ffd
Reketov <reshetovandrew@gmail.com>
2016-07-22 17:48:03 +0300

    Новый Класс

--- Modul2/src/HomeWork/date160721/ReshEx1.java
+++ Modul2/src/HomeWork/date160721/ReshEx1.java
@@ -4,7 +4,10 @@ package HomeWork.date160721;
  * Created by user on 22.07.16.
  */
 public class ReshEx1 {
-    public static void main(String[] args) {
-        
-    }
+
+        public static void main(String[] args) {
+            int q = 6;
+            System.out.println();
+        }
+
 }
