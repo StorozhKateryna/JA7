7c705cc6ca1e67d6af488219b40d616fbba1e7b7
pastrini <pbankomat@gmail.com>
2016-07-22 17:53:05 +0300

    new file

---
+++ Modul2/src/HomeWork/date160721/BorEx1.java
@@ -0,0 +1,15 @@
+package HomeWork.date160721;
+
+/**
+ * Created by user on 22.07.16.
+ */
+public class BorEx1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Guys!");
+
+
+    }
+
+
+}
