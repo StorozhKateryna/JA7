60471802a9c367aea2450a170d4455c191abf432
Reketov <reshetovandrew@gmail.com>
2016-07-25 13:06:18 +0300

    Решение задач

--- Modul2/src/HomeWork/date160721/ReshEx1.java
+++ Modul2/src/HomeWork/date160721/ReshEx1.java
@@ -2,6 +2,8 @@ package homework.data160721;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Есть строка «АА1458АВ». Вытянуть из нее в разные переменные текст (2-е переменные) и цифры (1-на
@@ -12,39 +14,20 @@ import java.util.Arrays;
  */
 public class ReshEx1 {
     public static void main(String[] args) {
-//        // create 2 char primitives ch1, ch2
-//        char ch1, ch2;
-//
-//        // assign values to ch1, ch2
-//        ch1 = 'A';
-//        ch2 = '9';
-//
-//        // create 2 boolean primitives b1, b2
-//        boolean b1, b2;
-//
-//        // check if ch1, ch2 are letter or not and assign results to b1, b2
-//        b1 = Character.isLetter(ch1);
-//        b2 = Character.isLetter(ch2);
-//
-//        String str1 = ch1 + " is a letter is " + b1;
-//        String str2 = ch2 + " is a letter is " + b2;
-//
-//        // print b1, b2 values
-//        System.out.println( str1 );
-//        System.out.println( str2 );
-        String str = "АА1458АВ";
-        int[] ch = new int[8];
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isLetter(str.charAt(i))) {
-                ch[i] = str.charAt(i);
-                System.out.println(str.charAt(i));
-            }
-            if (Character.isDigit(str.charAt(i))) {
-                System.out.println(str.charAt(i));
-            }
-
+        String s = "тел.: 234-59-45, тел.моб.: +38 067 333 33 33";
+        String[] str = new String[20];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = "";
         }
-        System.out.println(Arrays.toString(ch));
+        String myPattern = "[\\+]{0,1}([0-9]{1,3}[\\ ]{0,1}[\\-]{0,1}){3,5}";//любой одиночный символ который не являеться тем, что в скобках
+        Pattern mySplitter = Pattern.compile(myPattern);// Создаем паттерны
+        Matcher m = mySplitter.matcher(s);// Метод Matcher () проверяем строку на соответствие регулярному выражению
+        int count = 0;
 
+        while (m.find()) {
+            str[count] = m.group();
+            count++;
+        }
+        System.out.println(Arrays.toString(str));
     }
 }
